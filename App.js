353e418e1dcc9e738e6a1e9bdd6281a4f90e1b14
Budiman Rahardjo <budiman.rahardjo@computradetech.com>
2021-02-22 18:10:24 +0700

    TeslaClone

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './component/CarItem'
+import CarsList from './component/CarsList'
+import Header from './component/Header'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <CarsList />
+
       <StatusBar style="auto" />
     </View>
   );
---
+++ component/CarItem/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { View, Text, ImageBackground } from 'react-native'
+import styles from './styles'
+import StyledButton from '../StyledButton'
+
+const CarItem= (props) => {
+
+    const {name, tagline, image, taglineCTA} = props.car
+
+    return (
+        <View style={styles.carContainer}>
+        <ImageBackground 
+            source={image}
+            style={styles.image}/>
+
+        <View style={styles.titles}>
+          <Text style={styles.title}>{name}</Text>
+          <Text style={styles.subtitle}>{tagline}&nbsp;
+          <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
+          </Text>
+          
+        </View>
+
+        <View style={styles.buttonContainer}>
+        <StyledButton
+        type="primary" 
+        content={"Custom Order"} 
+        onPress={() => {
+            console.warn('custom order pressed')
+        }}/>
+
+        <StyledButton
+        type="secondary" 
+        content={"Existing Inventory"} 
+        onPress={() => {
+            console.warn('Existing Inventory pressed')
+        }}/>
+
+        </View>
+        
+      </View>
+    );
+};
+
+export default CarItem;
